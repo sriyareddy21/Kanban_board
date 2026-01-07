88d00b1024726c5def101dbae7c88282b6da46c0
sriyareddy21 <sriyareddy2020@gmail.com>
2026-01-08 00:54:04 +0530

    Fix deployment using docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Kanban_Board/",
   build: {
-    outDir: "docs"
+    outDir: "docs",
+    emptyOutDir: true
   },
   plugins: [react()]
 })
+
